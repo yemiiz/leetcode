3fb352ee1ee02fc04a86f413afa57c821a6902eb
yemiiz <415595323@qq.com>
2026-04-15 10:33:44 +0800

    修复一些格式bug

--- 2463. 最小移动总距离（困难）/solution.cpp
+++ 2463. 最小移动总距离（困难）/solution.cpp
@@ -1,11 +1,16 @@
 //核心：分配不得交叉
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+#include <climits>
+using namespace std;
 class Solution {
 public:
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
         sort(robot.begin(), robot.end());
         sort(factory.begin(), factory.end());//二维数组也可直接sort，先比较第一个元素再比较第二个元素
         int n = robot.size(), m = factory.size();//m为工厂数量
-        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, LONG_LONG_MAX / 2));
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, LLONG_MAX / 2));
         for (int j = 0; j <= m; j++) {
             dp[0][j] = 0;
         }
--- 2515. 到目标字符串的最短距离 （简单）/solution.cpp
+++ 2515. 到目标字符串的最短距离 （简单）/solution.cpp
@@ -1,14 +1,19 @@
-class Solution {
+#include <vector>
+#include <string>
+using namespace std;
+class Solution
+{
 public:
-    int closestTarget(vector<string>& words, string target, int startIndex) {
-        int n = words.size();
-        for(int i = 0;i<n;i++)
-        {
-            if(words[(startIndex+i)%n] == target || words[(startIndex-i+n)%n] == target)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
+    int closestTarget(vector<string> &words, string target, int startIndex)
+    {
+        int n = words.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (words[(startIndex + i) % n] == target || words[(startIndex - i + n) % n] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 };
