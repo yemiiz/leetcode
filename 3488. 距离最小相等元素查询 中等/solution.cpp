48cf9c2cceb35cbe52703cafde89ea0bdb3781b0
yemiiz <415595323@qq.com>
2026-04-17 17:39:25 +0800

    add 3488. 距离最小相等元素查询（哈希表存数据及映射的位置，找对应位置与其相近的位置）

---
+++ 3488. 距离最小相等元素查询 中等/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        unordered_map<int,vector<int>>mp;
+        for (int i = 0; i < nums.size(); ++i) {
+            mp[nums[i]].push_back(i);
+        }
+        /*
+        for(auto it = nums.begin();it!=nums.end();it++)
+        {
+            mp[nums[it]].push_back(it)这里不能push_back iteratorr; //push_back not insert
+        }
+        */
+        vector<int>ans;
+        for(auto it = 0;it!=queries.size();it++)
+        {
+            int ans1 = -1;
+           int q = queries[it]; 
+            if(mp[nums[q]].size()>=2) 
+            {
+                auto& vec = mp[nums[q]];    //！！！
+                int len = vec.size();
+
+                int pos = lower_bound(vec.begin(), vec.end(), q) - vec.begin(); //lower_bound找到第一个出现q的位置！！
+
+                int pre = vec[(pos-1+len)%len];
+                int nxt = vec[(pos+1)%len];
+                int n = nums.size();
+
+                ans1 = min( min(abs(q-pre), n-abs(q-pre)), min(abs(q-nxt), n-abs(q-nxt)) );
+            }
+            ans.push_back(ans1);
+        }
+        return ans;
+    }
+};
