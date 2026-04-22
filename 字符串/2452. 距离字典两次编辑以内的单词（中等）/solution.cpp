8962a90e10d7b66ed4da589976ddbb02f42356a4
yemiiz <415595323@qq.com>
2026-04-22 13:40:21 +0800

    增加暴力法

--- 字符串/2452. 距离字典两次编辑以内的单词（中等）/solution.cpp
+++ 字符串/2452. 距离字典两次编辑以内的单词（中等）/solution.cpp
@@ -1,3 +1,34 @@
+//1.暴力
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> ans; // 这里必须是 string 类型
+        for (string query : queries) {
+            bool found = false;
+            for (string s : dictionary) {
+                // 长度不同的字符串，无法通过2次编辑变成相同，直接跳过
+                if (query.size() != s.size()) continue;
+                
+                int dis = 0;
+                for (int i = 0; i < query.size(); ++i) {
+                    if (query[i] != s[i]) {
+                        dis++;
+                        // 差异数超过2时，提前终止比较
+                        if (dis > 2) break;
+                    }
+                }
+                if (dis <= 2) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                ans.push_back(query);
+            }
+        }
+        return ans;
+    }
+};
 //2.字典树
 struct TrieNode {
     TrieNode* child[26];
